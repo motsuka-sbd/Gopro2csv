fa406617adb42db185fa03020929f649a5c83e1e
unknown <motsuka@sbdautomotive.com>
2020-02-04 15:54:40 +0900

    commit

--- ConvMultMp4toCSV.py
+++ ConvMultMp4toCSV.py
@@ -5,6 +5,10 @@ import subprocess
 # get MP4 file list in the designated directory
 # this script needs argument for directory which contains MP4 file
 
+# Constants
+# AccGPS = 1000 # no need, as acccuracy does not matter to gpmd2csv
+# FixFPS = 3    # no need, as accuracy does not matter to gpmd2csv
+
 fileList = glob.glob(sys.argv + "/*.MP4")
 
 # convert mp4 to bin (recursive)
@@ -12,7 +16,14 @@ binList = []
 for f in fileList:
     binName = str(f) + ".bin"
     try:
-        subprocess.check_call("echo ffmpeg -y -i " + f + "-codec copy -map 0:3 -f rawvideo " + binName)
+        # ffprobe
+        # subprocess.check_call("ffprobe " + f)
+
+        # check which stream contains gpmd 
+
+
+        # ffmpeg
+        subprocess.check_call("ffmpeg -y -i " + f + "-codec copy -map 0:3 -f rawvideo " + binName)  # bad to have constants 0:3
     except:
         print("subprocess.check_call() in bin convert failed")
 
